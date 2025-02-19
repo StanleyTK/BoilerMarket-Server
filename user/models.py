87f272ead6dea738fadbc58141977d0c829f65a7
Varun Jasti <varunjasti@Varuns-MacBook-Air.local>
2025-02-19 15:07:02 -0500

    Migrated user model in a fucked up way

--- user/models.py
+++ user/models.py
@@ -4,11 +4,9 @@ class User(models.Model):
     uid = models.CharField(max_length=255, primary_key=True)
     email = models.EmailField()
     purdueEmail = models.EmailField(null=True, blank=True)
+    purdueEmailVerified = models.BooleanField(default=False)
     displayName = models.CharField(max_length=255)
     rating = models.FloatField(default=0)
     bio = models.TextField(null=True, blank=True)
     admin = models.BooleanField(default=False)
     banned = models.BooleanField(default=False)
-
-    class Meta:
-        db_table = "server_user"
