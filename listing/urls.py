3084a09cc9714e23ae5cc67c02541706fc3f3907
Andrew Bradley <andrewcbradley007@gmail.com>
2025-04-03 16:45:49 -0400

    Blocking now working on the saves and feed page

--- listing/urls.py
+++ listing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from listing.views import get_all_listings, create_listing, get_top_listings, get_listings_by_user, delete_listing, update_listing
-from listing.views import get_listing_by_lid, save_listing, unsave_listing, get_saved_listings
+from listing.views import get_listing_by_lid, save_listing, unsave_listing, get_saved_listings, get_top_listings_verified
 
 
 urlpatterns = [
@@ -9,10 +9,10 @@ urlpatterns = [
     path('getListing/<str:lid>/', get_listing_by_lid, name=" get_listing_by_lid"),
     path('create/', create_listing, name="create_listing"),
     path('homepage/', get_top_listings, name='get_top_listings'),
+    path('homepageVerified/', get_top_listings_verified, name='get_top_listings_verified'),
     path('delete/<str:listing_id>/', delete_listing, name='delete_listing'),
     path('update/<str:listing_id>/', update_listing, name='update_listing'),
     path('save/<int:listing_id>/', save_listing, name='save-listing'),
     path('unsave/<int:listing_id>/', unsave_listing, name='unsave-listing'),
     path('getSaved/', get_saved_listings, name='get-saved-listings')
-
 ]
